103481f8f01c25adae630043da862b972bdb90ac
AbdulrahmanAbozaid <diver72003@gmail.com>
2023-08-22 09:38:54 +0300

    done

--- convert_number.c
+++ convert_number.c
@@ -1,102 +1,118 @@
 #include "main.h"
+
+
 /**
- * print_octal - Prints an unsigned number in octal notation
- * @types: Lista of arguments
- * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags
- * @width: get width
- * @precision: Precision specification
- * @size: Size specifier
- * Return: Number of chars printed
+ * print_hex - prints hexadecimal
+ * @params: paras
+ * @ap: args
+ * Return: num of byes
  */
-int print_octal(va_list types, char buffer[],
-	int flags, int width, int precision, int size)
+
+int print_hex(va_list ap, params_t *params)
 {
+        unsigned long num;
+        int num_of_bytes = 0;
+        char *str;
 
-	int i = BUFF_SIZE - 2;
-	unsigned long int num = va_arg(types, unsigned long int);
-	unsigned long int init_num = num;
+        if (params->l_modifier)
+                num = (unsigned long int)va_arg(ap, unsigned long);
+        else if (params->h_modifier)
+                num = (unsigned short int)va_arg(ap, unsigned int);
+        else
+                num = (unsigned int)va_arg(ap, unsigned int);
 
-	UNUSED(width);
-
-	num = convert_size_unsgnd(num, size);
-
-	if (num == 0)
-		buffer[i--] = '0';
-
-	buffer[BUFF_SIZE - 1] = '\0';
-
-	while (num > 0)
-	{
-		buffer[i--] = (num % 8) + '0';
-		num /= 8;
-	}
-
-	if (flags & F_HASH && init_num != 0)
-		buffer[i--] = '0';
-
-	i++;
-
-	return (write_unsgnd(0, i, buffer, flags, width, precision, size));
-}
-/**
- * print_hexadecimal - Prints an unsigned number in hexadecimal notation
- * @types: Lista of arguments
- * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags
- * @width: get width
- * @precision: Precision specification
- * @size: Size specifier
- * Return: Number of chars printed
- */
-int print_hexadecimal(va_list types, char buffer[],
-	int flags, int width, int precision, int size)
-{
-	return (print_hexa(types, "0123456789abcdef", buffer,
-		flags, 'x', width, precision, size));
-}
-/**
- * print_binary - Prints an unsigned number
- * @types: Lista of arguments
- * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags
- * @width: get width.
- * @precision: Precision specification
- * @size: Size specifier
- * Return: Numbers of char printed.
- */
-int print_binary(va_list types, char buffer[],
-	int flags, int width, int precision, int size)
-{
-	unsigned int n, m, i, sum;
-	unsigned int a[32];
-	int count;
-
-	UNUSED(buffer);
-	UNUSED(flags);
-	UNUSED(width);
-	UNUSED(precision);
-	UNUSED(size);
-
-	n = va_arg(types, unsigned int);
-	m = 2147483648; /* (2 ^ 31) */
-	a[0] = n / m;
-	for (i = 1; i < 32; i++)
-	{
-		m /= 2;
-		a[i] = (n / m) % 2;
-	}
-	for (i = 0, sum = 0, count = 0; i < 32; i++)
-	{
-		sum += a[i];
-		if (sum || i == 31)
-		{
-			char z = '0' + a[i];
-
-			write(1, &z, 1);
-			count++;
-		}
-	}
-	return (count);
+        str = convert(num, 16, CONVERT_LOWERCASE | CONVERT_UNSIGNED, params);
+        if (params->hashtag_flag && l)
+        {
+                *--str = 'x';
+                *--str = '0';
+        }
+        params->unsign = 1;
+        return (num_of_bytes += print_number(str, params));
 }
 
+
+/**
+ * print_HEX - prints hexadecimal Upper
+ * @params: paras
+ * @ap: args
+ * Return: num of byes
+ */
+
+
+int print_HEX(va_list ap, params_t *params)
+{
+        unsigned long num;
+        int num_of_bytes = 0;
+        char *str;
+
+        if (params->l_modifier)
+                num = (unsigned long int)va_arg(ap, unsigned long);
+        else if (params->h_modifier)
+                num = (unsigned short int)va_arg(ap, unsigned int);
+        else
+                num = (unsigned int)va_arg(ap, unsigned int);
+
+        // not LOWER
+        str = convert(num, 16, CONVERT_UNSIGNED, params);
+        if (params->hashtag_flag && l)
+        {
+                *--str = 'X';
+                *--str = '0';
+        }
+        params->unsign = 1;
+        return (num_of_bytes += print_number(str, params));
+}
+
+
+/**
+ * print_binary - prints binary
+ * @params: paras
+ * @ap: args
+ * Return: num of byes
+ */
+
+
+int print_binary(va_list ap, params_t *params)
+{
+        unsigned int bin = va_arg(ap, unsigned int);
+        char *str = convert(bin, 2, CONVERT_UNSIGNED, params);
+        int num_of_bytes = 0;
+
+        if (params->hashtag_flag && bin)
+                *--str = '0';
+        params->unsign = 1;
+        return (num_of_bytes += print_number(str, params));
+}
+
+
+/**
+ * print_octal - prints base 8
+ * @params: paras
+ * @ap: args
+ * Return: num of byes
+ */
+
+
+int print_octal(va_list ap, params_t *params)
+{
+        unsigned long num;
+        int num_of_bytes = 0;
+        char *str;
+
+        if (params->l_modifier)
+                num = (unsigned long int)va_arg(ap, unsigned long);
+        else if (params->h_modifier)
+                num = (unsigned short int)va_arg(ap, unsigned int);
+        else
+                num = (unsigned int)va_arg(ap, unsigned int);
+
+        // not LOWER
+        str = convert(num, 8, CONVERT_UNSIGNED, params);
+        if (params->hashtag_flag && l)
+        {
+                *--str = '0';
+        }
+        params->unsign = 1;
+        return (num_of_bytes += print_number(str, params));
+}
--- string_fields.c
+++ string_fields.c
@@ -1,42 +1,30 @@
 #include "main.h"
 
 /**
- * get_precision - Calculates the precision for printing
- * @format: Formatted string in which to print the arguments
- * @i: List of arguments to be printed.
- * @list: list of arguments.
- *
- * Return: Precision.
+ * get_precision - get precision from fmt
+ * @p: fmt string
+ * @ap: args
+ * @params: parameters
+ * Return: num of byes
  */
-int get_precision(const char *format, int *i, va_list list)
+
+char *get_precision(char *p, params_t *params, va_list ap)
 {
-	int curr_i = *i + 1;
-	int precision = -1;
+        int pn = 0;
 
-	if (format[curr_i] != '.')
-		return (precision);
-
-	precision = 0;
-
-	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
-	{
-		if (is_digit(format[curr_i]))
-		{
-			precision *= 10;
-			precision += format[curr_i] - '0';
-		}
-		else if (format[curr_i] == '*')
-		{
-			curr_i++;
-			precision = va_arg(list, int);
-			break;
-		}
-		else
-			break;
-	}
-
-	*i = curr_i - 1;
-
-	return (precision);
+        if (*p != '.')
+                return (p);
+        pn++;
+        if (*p == '*')
+        {
+                pn = va_arg(ap, int);
+                p++;
+        }
+        else
+        {
+                while (_isdigit(*p))
+                        pn = pn * 10 + (*p++ - '0');
+        }
+        params->precision = pn;
+        return (p);
 }
-
